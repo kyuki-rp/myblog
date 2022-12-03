15c392deb868eb4cc57dbd47ffb581f98a1c706f
k-yukio <k-yuki@analytics-jp.com>
2022-12-03 09:22:45 +0000

    profile修正

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -20,7 +20,7 @@ const Profile = ({ data, location }) => {
       <Bio />
 
       <h2 style={{paddingTop:"1em"}}>業務範囲</h2>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>データ分析</li>
         <li>AIエンジニアリング</li>
         <li>システム開発</li>
@@ -30,13 +30,13 @@ const Profile = ({ data, location }) => {
 
       <h2 style={{paddingTop:"1em"}}>職歴</h2>
       <p>2018/04~： 2社で複数のプロジェクトに参画</p>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>SASを用いたビックデータ集計・分析</li>
         <li>Pythonを用いた自然言語データ、画像データ、時系列データの分析、AIモデル構築</li>
         <li>PySparkによる分散処理を利用したビックデータ解析システムの開発</li>
       </ul>
       <p>2021/11~： ITエンジニアとして活動</p>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>ドレスト光子発光現象の量子ウォークモデルを用いた数値シミュレーション</li>
         <li>Google Analyticsを用いたWebマーケティングデータ分析</li>
         <li>Gatsby, Django, Wordpressなどを用いたでサイト構築</li>
@@ -46,19 +46,19 @@ const Profile = ({ data, location }) => {
       <h2 style={{paddingTop:"1em"}}>学歴</h2>
       <p>2012/04~： 広島大学工学部（機械システム工学系）</p>
       <p>2016/04~： 広島大学工学研究科・物理工学専攻 博士課程前期</p>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>光誘起ドリフト効果を用いたセシウム同位体分離の理論解析、数値シミュレーション</li>
         <li>量子ウォークモデルを用いたセシウム同位体分離の数値シミュレーション</li>
       </ul>
 
       <h2 style={{paddingTop:"1em"}}>趣味</h2>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>コーディング</li>
         <li>読書</li>
       </ul>
 
       <h2 style={{paddingTop:"1em"}}>勉強中</h2>
-      <ul style={{paddingLeft:"3em"}}>
+      <ul>
         <li>関数型プログラミング</li>
         <li>ブロックチェーンエンジニアリング</li>
       </ul>
