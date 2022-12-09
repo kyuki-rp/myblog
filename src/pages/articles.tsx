0ada7129caceb1e19396a9eb703d475bfffdddb4
k-yukio <k-yuki@analytics-jp.com>
2022-12-09 11:21:03 +0000

    search更新

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -30,8 +30,8 @@ const Articles = ({ data, location }) => {
       <Seo title={siteTitle} />
 
       <h1 id="title">{siteTitle}</h1>
-      <ClientOnly style={{padding: "1rem 1rem 0 0"}}>
-          <Search />
+      <ClientOnly style={{padding: "1rem 1rem 0 1rem"}}>
+        <Search />
       </ClientOnly>
       <Tabs
         activeKey={tabKey}
