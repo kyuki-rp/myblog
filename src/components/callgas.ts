a3471947cbca2e909ea5bfa3e80b3b2a789b7b1a
k-yukio <k-yuki@analytics-jp.com>
2023-10-18 04:51:08 +0000

    AGSデプロイ更新

--- src/components/callgas.ts
+++ src/components/callgas.ts
@@ -3,7 +3,7 @@ import * as React from "react"
 
 export const CallGAS = (path, name, email, comment) => {
 
-    const URL = "https://script.google.com/macros/s/AKfycbyp8BzjaSKXc24AY2jCKV7ve27h99F-oo15mQ0jpYCoTvlKkQ6BYV4lBIjFusoLCDI6/exec";
+    const URL = "https://script.google.com/macros/s/AKfycbwN4SC4fR7RDKeY589TBLiEsh4mSGp73Cv3OF3MKEZBpeZX6jEuP-OhWbXIIKmKopZO/exec";
     let SendDATA = {
       "form-path" : path,
       "form-name" : name,
